ea1668b6a9d2ce4e4b7aba04d84bc819ee3b8f89
Medí Galofré <medigp@gmail.com>
2023-03-10 00:14:17 +0100

    CMA-3 : Redisseny de la pantalla About

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from 'styled-components'
+import { DeviceSize } from "/src/data/responsive"
 import Seo from "/src/components/SEO"
 import Layout from '/src/components/layout/Layout'
 import { getTranslatedText } from "/src/components/translate/TranslateText";
@@ -29,11 +30,14 @@ const About = ({data}) => {
 
       return (
         <Paragraph key={index}>
-          {text &&
+          <IconElement className='p-icon'></IconElement>
+          <ParagraphWrapper className='p-wrapper'>
+          {text &&            
             <ParagraphText
               dangerouslySetInnerHTML={{__html:text}}
             ></ParagraphText>
           }
+          </ParagraphWrapper>
           {definedImage &&
             <GatsbyImage
               image={definedImage}
@@ -111,13 +115,125 @@ export const query = graphql`
 const LayoutContentWrapper = styled.div`
   max-width: var(--max-content-width);
   margin:auto;
+  position:relative;
 `
 
 const LayoutTextWrapper = styled(LayoutContentWrapper)`
   padding-top:2rem;
 `
 const Paragraph = styled.div`
+  display:flex;
+  padding-bottom:3rem;
+  position:relative;
+  z-index:0;
+
+  .p-icon{
+    position:absolute;
+    width:10px;
+    height:10px;
+    border-radius:10px;
+    content:'';
+    background:white;
+    top:1em;
+    left:calc(1rem - 6px);
+    border:2px solid var(--primary-link-hover-color);
+    z-index:3;
+    transition: all 0.5s ease;
+  }
+
+  &:hover .p-icon{
+    border-color: var(--alternative-color);;
+    transform:scale(2);
+  }
+
+  &::before,
+  &::after{
+    position:absolute;
+    width:0;
+    height:100%;
+    content:'';
+    top:1em;
+    left:1rem;
+    border:1px dashed var(--primary-link-hover-color);
+    xx-border-style:solid;
+    z-index:1;
+    transition: all 0.5s ease;
+  }
+  &::after{
+    display:none;
+    z-index:2;
+    height:0;
+    border-color:var(--alternative-color);
+    transition: all 1s ease-out;
+  }
+  &:hover::after{
+    height:100%;
+  }
+
+  &:last-of-type::before,
+  &:last-of-type::after{
+    display:none;
+  }
+
+  div.p-wrapper {
+    margin-top:0;
+    transform:translateY(-0.5em);
+  }
+
+  div.p-wrapper p{
+    padding-left: 3rem;
+    transition: all 0.5s ease;
+  }
+
+  &:hover div.p-wrapper p{
+    transform: translateX(-0.5em);
+  } 
+
+  @media ( min-width : ${DeviceSize.mobile}px ){
+    text-align:right;
+
+    &::before,
+    &::after{
+      left:50%;
+    }
   
+    .p-icon{
+      left:calc(50% - 6px);
+    }
+    
+    div.p-wrapper {
+      width: 50%;
+    }
+    div.p-wrapper p{
+      padding-left:0;
+      padding-right: 3rem;
+    }
+
+    &:hover div.p-wrapper p{
+      transform: translateX(0.5em);
+    } 
+
+    &:nth-of-type(2n){
+      justify-content: end;
+      text-align:left;
+      
+      div.p-wrapper p{
+        padding-left:3rem;
+        padding-right: 0;
+      }
+      &:hover div.p-wrapper p{
+        transform: translateX(-0.5em);
+      }
+    }
+
+  }
+
+`
+const ParagraphWrapper = styled.div`
+
+`
+
+const IconElement = styled.div`
 `
 const ParagraphText = styled.p`
   
