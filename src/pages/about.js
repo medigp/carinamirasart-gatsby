a91fde913d9cc089644199339b906cfb5cb56f39
Medí Galofré <medigp@gmail.com>
2023-03-11 21:13:22 +0100

    CMA-3 : Redisseny de la pantalla about - Afegir títols de paràgrafs; - Afegir títol i línia de cronologia

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,11 +17,13 @@ const About = ({data}) => {
     const title = getTranslatedText('About.title',lang)
     const subtitle = getTranslatedText('About.subtitle', lang)
 
+    const chronologyTitle = getTranslatedText('Chronology',lang)
+
     const getParagraph = (paragraph, index) => {
       if(!paragraph)
         return "";
 
-      const {text , image} = paragraph
+      const {title, text , image} = paragraph
 
       if(!text && !image)
         return "";
@@ -30,6 +32,12 @@ const About = ({data}) => {
 
       return (
         <Paragraph key={index}>
+          <ParagraphTitle
+            className='p-title'>
+            <ParagraphH3 
+              dangerouslySetInnerHTML={{__html:(title || "Títol")}}
+            ></ParagraphH3>
+          </ParagraphTitle>
           <IconElement className='p-icon'></IconElement>
           <ParagraphWrapper className='p-wrapper'>
           {text &&            
@@ -60,6 +68,7 @@ const About = ({data}) => {
             </LayoutContentWrapper>
 
             <LayoutTextWrapper>
+                <ChronologyTytle>{chronologyTitle}</ChronologyTytle>
                 {paragraphs 
                   && paragraphs.map((paragraph, index) => 
                     getParagraph(paragraph, index))
@@ -105,6 +114,7 @@ export const query = graphql`
         description
       }
       paragraphs {
+        title
         text
       }
 
@@ -122,10 +132,26 @@ const LayoutTextWrapper = styled(LayoutContentWrapper)`
   padding-top:2rem;
 `
 const Paragraph = styled.div`
-  display:flex;
-  padding-bottom:3rem;
+  display:grid;
+  grid-template-columns: 1fr;
+  grid-template-areas : 
+    "title"
+    "text";
+  padding: 1.5rem 0;
   position:relative;
   z-index:0;
+
+  h3{ 
+    color: var(--alternative-color);
+    transition: all 0.5s ease;
+    padding-left: 3rem;
+    line-height: 2rem;
+    margin:0;
+  }
+
+  &:hover h3{
+    font-size: 2rem;
+  }
 
   .p-icon{
     position:absolute;
@@ -134,7 +160,7 @@ const Paragraph = styled.div`
     border-radius:10px;
     content:'';
     background:white;
-    top:1em;
+    top:calc(1.5rem + 0.5rem);
     left:calc(1rem - 6px);
     border:2px solid var(--primary-link-hover-color);
     z-index:3;
@@ -142,46 +168,33 @@ const Paragraph = styled.div`
   }
 
   &:hover .p-icon{
-    border-color: var(--alternative-color);;
+    border-color: var(--alternative-color);
     transform:scale(2);
   }
 
-  &::before,
-  &::after{
+  &::before{
     position:absolute;
     width:0;
     height:100%;
     content:'';
-    top:1em;
+    top:2rem;
     left:1rem;
     border:1px dashed var(--primary-link-hover-color);
-    xx-border-style:solid;
     z-index:1;
     transition: all 0.5s ease;
   }
-  &::after{
-    display:none;
-    z-index:2;
-    height:0;
-    border-color:var(--alternative-color);
-    transition: all 1s ease-out;
-  }
-  &:hover::after{
-    height:100%;
-  }
 
-  &:last-of-type::before,
-  &:last-of-type::after{
+  &:last-of-type::before{
     display:none;
   }
 
   div.p-wrapper {
-    margin-top:0;
-    transform:translateY(-0.5em);
+    margin: 0;
   }
 
   div.p-wrapper p{
     padding-left: 3rem;
+    margin-top:0;
     transition: all 0.5s ease;
   }
 
@@ -190,45 +203,98 @@ const Paragraph = styled.div`
   } 
 
   @media ( min-width : ${DeviceSize.mobile}px ){
-    text-align:right;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas : "title text";
 
-    &::before,
+    &:nth-of-type(2n){
+      grid-template-areas : "text title";
+    }
+
     &::after{
+      display:block;
+      border:0px;
+      background:red;
+      filter: blur(2rem);
+    }
+
+    &::before{
       left:50%;
     }
-  
+
+    &:first-of-type::before{
+      top: 50%; 
+    }
+
+    &:last-of-type::before{
+      display:block;
+      top:0;
+      height: 50%;
+    }
+
     .p-icon{
       left:calc(50% - 6px);
+      top: calc(50% - 6px);
     }
+
+    .p-wrapper,
+    .p-title{
+      display:flex;
+      align-items: center; 
+      justify-content: center;
+      text-align:center;
+    }
+
+    .p-wrapper p,
+    .p-title h3{
+      max-width: 80%;
+    }
+
     
-    div.p-wrapper {
-      width: 50%;
-    }
     div.p-wrapper p{
-      padding-left:0;
-      padding-right: 3rem;
+      padding:0;
     }
 
     &:hover div.p-wrapper p{
       transform: translateX(0.5em);
-    } 
+    }
 
-    &:nth-of-type(2n){
-      justify-content: end;
-      text-align:left;
-      
-      div.p-wrapper p{
-        padding-left:3rem;
-        padding-right: 0;
-      }
+    &:nth-of-type(2n){     
       &:hover div.p-wrapper p{
         transform: translateX(-0.5em);
       }
     }
 
+    h3{
+      opacity:0.15;
+      font-size:8rem;
+      line-height: 8rem;
+      vertical-align:top;
+      margin:0;
+      padding: 0;
+    }
+
+    &:hover h3{
+      opacity: 0.75;
+      font-size:8rem;
+      transform:scale(1.2);
+    }
   }
 
 `
+
+const ChronologyTytle = styled.h2`
+  @media ( min-width : ${DeviceSize.mobile}px ){
+    text-align: center;
+  }
+`
+
+const ParagraphTitle = styled.div`
+  grid-area : title;
+  xx-overflow:hidden;
+`
+const ParagraphH3 = styled.h3`
+`
+
 const ParagraphWrapper = styled.div`
 
 `
@@ -236,7 +302,7 @@ const ParagraphWrapper = styled.div`
 const IconElement = styled.div`
 `
 const ParagraphText = styled.p`
-  
+  grid-area : text;
 `
 
 export default About;
